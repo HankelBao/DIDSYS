640e96b5580bb652690e3e4c8424ef57200951f6
Hankel BAO <Hankel BAO>
2017-01-21 12:23:10 +0800

    finish check(if you score again, then score won't be recorded)

--- database/scoreSubmit.php
+++ database/scoreSubmit.php
@@ -7,7 +7,15 @@
     $scorer = $_POST['scorer'];
     require('databaseConnect.php');
     for ($x = 0; $x < count($score_pos); $x++) {
-        mysqli_query($connection, "INSERT INTO `serial` (`serial_id`, `date`, `class_id`, `subject_id`, `scorer_id`, `score`, `score_time`, `description`) VALUES (NULL, '".$score_date."','".$score_cla[$x]."','".$score_sub[$x]."','".$scorer."','".$score_pos[$x]."','".$score_time."','');");
+        $result = mysqli_query($connection, 'SELECT * FROM serial where date = "'.date('y-m-d',time()).'" and subject_id = '.$score_sub[$x].' and class_id = '.$score_cla[$x]);
+        if($result) {
+            $row = mysqli_fetch_array($result);
+            if (!$row)
+                mysqli_query($connection, "INSERT INTO `serial` (`serial_id`, `date`, `class_id`, `subject_id`, `scorer_id`, `score`, `score_time`, `description`) VALUES (NULL, '".$score_date."','".$score_cla[$x]."','".$score_sub[$x]."','".$scorer."','".$score_pos[$x]."','".$score_time."','');");
+        } else {
+            echo "null";
+        } 
+            
     }
     require('databaseClose.php');
 ?>
--- home-AddRecord.php
+++ home-AddRecord.php
@@ -33,9 +33,9 @@
             }
             session_start();
             echo "<form action='database/scoreSubmit.php' method='POST'>";
-            echo "<input name='score_date' value='".date('y-m-d',time())."' style='display:no'\>";
-            echo "<input name='score_time' value='".date('y-m-d h:i:s',time())."' style='display:no'\>";
-            echo "<input name='scorer' value='".$_SESSION['scorer_id']."' style='display:no'\>";
+            echo "<input name='score_date' value='".date('y-m-d',time())."' style='display:none'\>";
+            echo "<input name='score_time' value='".date('y-m-d h:i:s',time())."' style='display:none'\>";
+            echo "<input name='scorer' value='".$_SESSION['scorer_id']."' style='display:none'\>";
             for($i=0; $i<count($class); $i++) {
                 echo "<tr>";
                 echo "<th>";
@@ -44,8 +44,8 @@
                 for ($j = 0; $j < count($subject); $j++) {
                     echo "<th>";
                     echo "<input name='score_pos[]' type='text'/>";
-                    echo "<input name='score_cla[]' value='".$cla_id[$i]."' style='display:non'\>";
-                    echo "<input name='score_sub[]' value='".$sub_id[$j]."' style='display:non'\>";
+                    echo "<input name='score_cla[]' value='".$cla_id[$i]."' style='display:none'\>";
+                    echo "<input name='score_sub[]' value='".$sub_id[$j]."' style='display:none'\>";
                     echo "</th>";
                 }
                 echo "</tr>";
