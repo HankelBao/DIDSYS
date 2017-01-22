7eda664c43ad03b16012cfcaa58c852404221166
Hankel BAO <Hankel BAO>
2017-01-22 13:10:33 +0800

    null framework

--- index.php
+++ index.php
@@ -9,34 +9,6 @@
 
     <body>
         <script type="text/javascript" src="js/jquery.min.js"></script>
-        <!--<div class="top-nav">
-            <label class="title-label">SFLS Discipline Inspection Department</label>
-            <label class="title-chi">苏州外国语学校纪检部评分官网</label>
-            <label class="signature-label">Powered by HankelBao</label>
-        </div>-->
-    
-        <div class="intro-page">
-            
-            <div class="signature-div">    
-                <label class="signature-label">Powered&nbsp;&nbsp;by&nbsp;&nbsp;HankelBao</label>
-            </div>
-            
-            <div class="center-div">
-                <label class="title-did">SFLS Discipline Inspection Department</label></br>
-                <label class="illustration-div">苏州外国语学校纪检部</label></br>
-                <label class="title-year"><b>2 0 1 7</b></label></br>
-                <label class="title-month">         
-                    <b>J&nbsp;&nbsp;a&nbsp;&nbsp;n&nbsp;&nbsp;u&nbsp;&nbsp;a&nbsp;&nbsp;r&nbsp;&nbsp;y</b>
-                </label>
-                </br>
-                <div style="signin" style="float:right">
-                    <form action="database/signin.php" method="POST">
-                        <input placeholder="&nbsp;username" class="input-text" name="username" type="text"></br>
-                        <input placeholder="&nbsp;password" class="input-text" name="password" type="password"></br>
-                        <button class="submit-button" type="submit">Sign In</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+  
     </body>
 </html>
