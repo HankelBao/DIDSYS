38934372e4ea47b216dfcf45ad2f29ca7ee95948
Hankel BAO <Hankel BAO>
2017-01-23 12:08:42 +0800

    div finish

--- home-scorer.php
+++ home-scorer.php
@@ -4,7 +4,6 @@
         <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
         <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css">
         <link type="text/css" rel="stylesheet" href="theme/layout.css"/>
-        <link type="text/css" rel="stylesheet" href="theme/home.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     </head>
 
@@ -12,15 +11,10 @@
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <div class="nav-div">
             <img src="SFLS.jpg" width="70px" height="70px"/>
-            <a href="index.php" class="link-div active">SFLS DI Department</a>
+            <a href="index.php" class="link-div">SFLS DI Department</a>
             <a href="home-scoreBoard.php" class="link-div">ScoreBoard</a>
             <a href="home-history.php" class="link-div">History</a>
-            <a href="home-scorer.php" class="link-div">Scorer</a>
-        </div>
-        <div class="intro-page">
-            <div class="title-div">Welcome to SFLS Discipline Inspection Department</div>
-            <div class="subtitle-div">A just, powerful department scoring reasonable marks</div>
-            <embed width="100%" height="100%" src="file:///C:/Users/hanke/Desktop/纪检部扣分细则.pdf" type="application/pdf" style="margin:0px;padding:0px;overflow:hidden;display:block;">
+            <a href="home-scorer.php" class="link-div active">Scorer</a>
         </div>
     </body>
 </html>
