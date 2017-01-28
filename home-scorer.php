08c94c34fed8c79c8729ca49989b9b9df868e590
Hankel BAO <Hankel BAO>
2017-01-28 12:07:12 +0800

    database rebuilt becuase of the name rule

--- home-scorer.php
+++ home-scorer.php
@@ -20,10 +20,43 @@
             <a href="home-history.php" class="link-div">History</a>
             <a href="home-scorer.php" class="link-div active">Scorer</a>
         </div>
-        <div class="login-div">
+        <div class="pos-center-div">
             <div class="title-div">Welcome! <?php echo $_SESSION['scorer_name']; ?> </div>
             <div class="subtitle-div">It's <?php echo date('y年m月d日',time())?> today. Your score will be updated to scoreboard at 5:30</div>
             <div class="warntitle-div">You must score justful and careful!!</div>
+        </div>
+        <div class="pos-center-div">
+       <?php
+            require('database/databaseConnect.php');
+            require('database/subTableHead.php');
+            require('database/claLoad.php');
+
+            //session_start();
+            echo "<form action='database/scoreSubmit.php' method='POST'>";
+            echo "<input name='score_date' value='".date('y-m-d',time())."' style='display:none'\>";
+            echo "<input name='score_time' value='".date('y-m-d h:i:s',time())."' style='display:none'\>";
+            echo "<input name='scorer' value='".$_SESSION['scorer_id']."' style='display:none'\>";
+            for($i=0; $i<count($class); $i++) {
+                echo "<tr>";
+                echo "<th>";
+                echo $class[$i];
+                echo "</th>";
+                for ($j = 0; $j < count($subject); $j++) {
+                    echo "<th>";
+                    echo "<input class='input-def' name='score_pos[]' type='text'/>";
+                    echo "<input name='score_cla[]' value='".$cla_id[$i]."' style='display:none'\>";
+                    echo "<input name='score_sub[]' value='".$sub_id[$j]."' style='display:none'\>";
+                    echo "</th>";
+                }
+                echo "</tr>";
+            }
+
+            require('database/subTableEnd.php');
+            echo "<button type='submit'>Submit</button>";
+            echo "</form>";
+            
+            require('database/databaseClose.php');
+        ?>
         </div>
     </body>
 </html>
