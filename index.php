a98606f732e2aa99b4f5b1ff83fd18bd92498722
Hankel BAO <Hankel BAO>
2017-01-31 14:26:55 +0800

    0

--- index.php
+++ index.php
@@ -17,27 +17,5 @@
             <a href="home-history.php" class="link-div">History</a>
             <a href="home-scorer.php" class="link-div">Scorer</a>
         </div>
-        <div class="intro-page">
-            
-            <div class="signature-div">    
-                <label class="signature-label">Powered&nbsp;&nbsp;by&nbsp;&nbsp;HankelBao</label>
-            </div>
-            
-            <div class="center-div">
-                <label class="title-did">SFLS Discipline Inspection Department</label></br>
-                <label class="illustration-div">苏州外国语学校纪检部</label></br>
-                <label class="title-year"><b>2 0 1 7</b></label></br>
-                <label class="title-month">         
-                    <b>J&nbsp;&nbsp;a&nbsp;&nbsp;n&nbsp;&nbsp;u&nbsp;&nbsp;a&nbsp;&nbsp;r&nbsp;&nbsp;y</b>
-                </label>
-                </br>
-                <div style="signin" style="float:right">
-                    <form action="database/signin.php" method="POST">
-                        <input placeholder="&nbsp;username" class="input-text" name="username" type="text"></br>
-                        <input placeholder="&nbsp;password" class="input-text" name="password" type="password"></br>
-                        <button class="submit-button" type="submit">Sign In</button>
-                    </form>
-                </div>
-            </div>
     </body>
 </html>
